3c3ad016ae96041cbd5f1a201530cb0cbb37579c
Hasan Basri <85760132+DargaHasanBasri@users.noreply.github.com>
2025-07-15 16:25:17 +0300

    Create the project basic structure

---
+++ os_ulas_admin_panel/lib/export.dart
@@ -0,0 +1,2 @@
+export 'package:flutter/material.dart';
+export 'package:os_ulas_admin_panel/ui/screens/dashboard/dashboard_screen.dart';
---
+++ os_ulas_mobile/lib/export.dart
@@ -0,0 +1,3 @@
+export 'package:flutter/material.dart';
+export 'package:os_ulas_mobile/ui/screens/splash/splash_screen.dart';
+export 'package:os_ulas_mobile/ui/screens/home/home_screen.dart';
