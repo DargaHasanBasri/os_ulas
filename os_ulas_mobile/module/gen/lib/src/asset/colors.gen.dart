d77450dbb6252531507e2f99889b7e08bb80b2d4
Hasan Basri <85760132+DargaHasanBasri@users.noreply.github.com>
2025-07-17 00:55:28 +0300

    Add colors and icons and try them out

--- os_ulas_mobile/module/gen/lib/src/asset/colors.gen.dart
+++ os_ulas_mobile/module/gen/lib/src/asset/colors.gen.dart
@@ -13,8 +13,50 @@ import 'package:flutter/material.dart';
 class ColorName {
   ColorName._();
 
+  /// Color: #F3F3F3
+  static const Color aquaHaze = Color(0xFFF3F3F3);
+
   /// Color: #000000
   static const Color black = Color(0xFF000000);
+
+  /// Color: #F0A58E
+  static const Color blush = Color(0xFFF0A58E);
+
+  /// Color: #A9A1C9
+  static const Color coldPurple = Color(0xFFA9A1C9);
+
+  /// Color: #34C759
+  static const Color darkMint = Color(0xFF34C759);
+
+  /// Color: #989692
+  static const Color grey = Color(0xFF989692);
+
+  /// Color: #111827
+  static const Color mirage = Color(0xFF111827);
+
+  /// Color: #6B7280
+  static const Color paleSky = Color(0xFF6B7280);
+
+  /// Color: #613EEA
+  static const Color purpleBlue = Color(0xFF613EEA);
+
+  /// Color: #FF3B30
+  static const Color redOrange = Color(0xFFFF3B30);
+
+  /// Color: #9DB2CE
+  static const Color rockBlue = Color(0xFF9DB2CE);
+
+  /// Color: #9CA3AF
+  static const Color santaGrey = Color(0xFF9CA3AF);
+
+  /// Color: #CACACA
+  static const Color silver = Color(0xFFCACACA);
+
+  /// Color: #6C7072
+  static const Color smokeyGrey = Color(0xFF6C7072);
+
+  /// Color: #616161
+  static const Color stormDust = Color(0xFF616161);
 
   /// Color: #FFFFFF
   static const Color white = Color(0xFFFFFFFF);
