f96dc4c8326677da21afda1870d9a7b622017d82
Hasan Basri <85760132+DargaHasanBasri@users.noreply.github.com>
2025-09-28 20:58:25 +0300

    Add app routes

---
+++ os_ulas_mobile/lib/routes/app_route_names.dart
@@ -0,0 +1,8 @@
+final class AppRouteNames {
+  AppRouteNames._();
+
+  static const String splash = '/splash';
+  static const String onboarding = '/onboarding';
+  static const String welcome = '/welcome';
+  static const String home = '/home';
+}
---
+++ os_ulas_mobile/lib/routes/export.dart
@@ -0,0 +1,7 @@
+export 'package:go_router/go_router.dart';
+export 'package:os_ulas_mobile/routes/app_route_names.dart';
+export 'package:flutter/material.dart';
+export 'package:os_ulas_mobile/ui/screens/home/home_screen.dart';
+export 'package:os_ulas_mobile/ui/screens/onboarding/onboarding_main_screen.dart';
+export 'package:os_ulas_mobile/ui/screens/splash/splash_screen.dart';
+export 'package:os_ulas_mobile/ui/screens/welcome/welcome_screen.dart';
